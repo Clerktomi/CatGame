48c3e528f2f4fcbe0a7c321df2391fc80df42731
Szabó Tamás <162686525+Clerktomi@users.noreply.github.com>
2025-11-18 22:08:05 +0100

    alap function

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ let CatFoodSzoveg = "Kaja: ";
 CatHpLabel.textContent = CatHpSzoveg + CatHpData + "%";
 CatFoodLabel.textContent = CatFoodSzoveg + CatFoodData + "%";
 Money.textContent = MoneyData + "$";
+
+function CatPat(){
+    MoneyData += 10;
+    Money.textContent = MoneyData + "$";
+}
+
+if (MoneyData > 30){
+    function CoffeBuy(){
+        MoneyData -= 30;
+        Money.textContent = MoneyData + "$";
+        }
+}
+
+if (MoneyData > 100){
+    function BeefBuy(){
+        MoneyData -= 100;
+        Money.textContent = MoneyData + "$";
+        }
+}
+
+if (MoneyData > 50){
+    function ToiltBuy(){
+        MoneyData -= 50;
+        Money.textContent = MoneyData + "$";
+        }
+}
